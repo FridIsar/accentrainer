c8ea0e775ddf71ab1739a631baa41f6c04c60d11
Isar <isarfridriksson@gmail.com>
2023-04-10 22:18:13 +0000

    escaping apostrophe for build

--- components/speech-recognition.tsx
+++ components/speech-recognition.tsx
@@ -48,7 +48,7 @@ const Dictaphone = () => {
       </div>
       <div className="flex items-center border-b border-teal-500 py-2">
       <SpeakerWaveIcon className="h-8"/>
-        <div className="appearance-none bg-transparent border-none w-full text-gray-100 mr-3 py-1 px-2 leading-tight focus:outline-none"><span className="font-bold">What I'm hearing: </span> {`${transcript}`}</div>
+        <div className="appearance-none bg-transparent border-none w-full text-gray-100 mr-3 py-1 px-2 leading-tight focus:outline-none"><span className="font-bold">What I&apos;m hearing: </span> {`${transcript}`}</div>
         <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" onClick={resetTranscript} type="button">
           Reset
         </button>
